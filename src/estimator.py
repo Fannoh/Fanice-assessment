1ab9fceb3c90cf8e7dded84146cd23e6075ebfa9
Fannoh <63151485+Fannoh@users.noreply.github.com>
2020-04-14 18:03:02 +0300

    Update estimator.py

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,32 @@
 def estimator(data):
+  region
+  name="Africa"
+  Period type="days"
+  Time to lapse="30"
+  Reported cases to date=15471
+  population =133315749
+  {
+estimate:{
+    "impact":
+            "currently infected = 154710
+            "infection by requested time" = 158423040   #1024*currently infected
+            "Severe cases by requested time=
+            "Hospital beds by requested time"=
+            "cases for ICUBy Requested Time"=
+            "cases ForVentilatorsBy Requested Time"=
+            
+}
+    "severe Impact" {
+      "currentlyInfected =773550
+      "Infection By Requested Time"=792115200
+      "Severe Cases By Requested Time"=
+      "Hospital Beds By Requested Time"=
+      "Cases for ICUBy Requested Time"=
+      "Cases for ventilators"=
+      "Dollars inflight"=
+                
+            
+  
+  
+  
   return data
