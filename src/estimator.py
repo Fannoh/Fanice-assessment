2d6ad98f1b0000520852dea347ff61708649b3fc
Fannoh <63151485+Fannoh@users.noreply.github.com>
2020-04-14 22:06:44 +0300

    Update estimator.py

--- src/estimator.py
+++ src/estimator.py
@@ -1,6 +1,8 @@
 def estimator(data):
   region
   name="Africa"
+  avgAge:19.7
+  avgDailyIncomeInUSD:5
   Period type="days"
   Time to lapse="30"
   Reported cases to date=15471
@@ -9,20 +11,20 @@ def estimator(data):
 estimate:{
     "impact":
             "currently infected = 154710
-            "infection by requested time" = 158423040   #1024*currently infected
-            "Severe cases by requested time=
-            "Hospital beds by requested time"=
-            "cases for ICUBy Requested Time"=
-            "cases ForVentilatorsBy Requested Time"=
+            "infection by requested time" = 15842304   #1024*currently infected
+            "Severe cases by requested time"=2376346
+            "Hospital beds by requested time"=831721
+            "cases for ICUBy Requested Time"=792115
+            "cases ForVentilatorsBy Requested Time"=316846
             
 }
     "severe Impact" {
-      "currentlyInfected =773550
+      "currentlyInfected" =773550
       "Infection By Requested Time"=792115200
-      "Severe Cases By Requested Time"=
-      "Hospital Beds By Requested Time"=
-      "Cases for ICUBy Requested Time"=
-      "Cases for ventilators"=
+      "Severe Cases By Requested Time"=7921152000
+      "Hospital Beds By Requested Time"=110896128
+      "Cases for ICUBy Requested Time"=39605760
+      "Cases for ventilators"=15842304
       "Dollars inflight"=
                 
             
@@ -30,3 +32,22 @@ estimate:{
   
   
   return data
+     estimate:{
+    "impact":
+            "currently infected" = 154710
+            "infection by requested time" = 15842304   #1024*currently infected
+            "Severe cases by requested time"=2376346
+            "Hospital beds by requested time"=-1544625
+            "cases for ICUBy Requested Time"=792115
+            "cases ForVentilatorsBy Requested Time"=316846
+            "DollarsInFlight"=2019893760
+            
+}
+    "severe Impact" {
+      "currentlyInfected" =773550
+      "Infection By Requested Time"=792115200
+      "Severe Cases By Requested Time"=118817280
+      "Hospital Beds By Requested Time"=-77231232
+      "Cases for ICUBy Requested Time"=39605760
+      "Cases for ventilators"=15842304
+      "Dollars inflight"= 100994688000
